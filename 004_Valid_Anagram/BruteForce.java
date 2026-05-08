a3eadf51b3f3a317eaa1aa93ae57b4784922e700
lavanya-raheja <95681022+lavanya-raheja@users.noreply.github.com>
2026-05-08 23:30:47 +0530

    Update BruteForce.java

--- 004_Valid_Anagram/BruteForce.java
+++ 004_Valid_Anagram/BruteForce.java
@@ -1,1 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) { 
+            return false; 
+        }
 
+        char[] ss = s.toCharArray(); 
+        char[] tt = t.toCharArray();
+
+        Arrays.sort(ss); 
+        Arrays.sort(tt); 
+        
+        return Arrays.equals(ss,tt); 
+    }
+}
+
+/* Time Complexity: O(nlogn)
+We basically just convert both strings to arrays and sort and compare them. If they are anagrams, they should be equal. */
